d6e2ee16e963c4e2617651788e754a0e18a32a73
Nik Todorov <ntodorov@gmail.com>
2025-10-22 09:34:51 -0600

    setting custom url

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form } from '@formio/react';
 import './App.css';
 
 function App() {
+  const [formUrl, setFormUrl] = useState(
+    'https://formio-api-dev.azurewebsites.net/tqmhwzomotajfzu/manage/view/#/form/publicaerform1?header=1&reset=1&host=formio-api-dev.azurewebsites.net&protocol=https'
+  );
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Formio PWA</h1>
-        <Form src="https://formio.github.io/formio.js/app/examples/form.json" onSubmit={(submission) => console.log(submission)} />
+        <div>
+          <input
+            type="text"
+            value={formUrl}
+            onChange={(e) => setFormUrl(e.target.value)}
+            placeholder="Enter Form.io URL"
+            style={{ marginRight: '10px', padding: '5px' }}
+          />
+          <button
+            onClick={() => setFormUrl(formUrl)}
+            style={{ padding: '5px 10px' }}
+          >
+            Load Form
+          </button>
+        </div>
+        <Form
+          key={formUrl}
+          src={formUrl}
+          onSubmit={(submission) => console.log(submission)}
+        />
       </header>
     </div>
   );
